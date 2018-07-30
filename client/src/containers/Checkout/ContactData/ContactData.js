401e43bfb00c9dd36d6662355a521b513f12ac33
Nick Sladden <nickoliasxii@gmail.com>
2018-07-30 09:01:46 -0500

    orders output complete

--- client/src/containers/Checkout/ContactData/ContactData.js
+++ client/src/containers/Checkout/ContactData/ContactData.js
@@ -20,7 +20,7 @@ class ContactData extends Component {
     event.preventDefault();
     this.setState({ loading: true });
     const order = {
-      ingredients: this.state.ingredients,
+      ingredients: this.props.ingredients,
       price: this.props.price, //ideally, you would still check this value on the server to prevent tampering
       customer: {
         name: 'Slad the Lad',
