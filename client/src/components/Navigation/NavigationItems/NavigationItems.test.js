1793bc073e96e8d82e0cbb27b25883e5be4181ae
Nick Sladden <nickoliasxii@gmail.com>
2018-08-17 21:29:39 -0500

    basic tests

--- client/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ client/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -25,5 +25,9 @@ describe('<NavigationItems />', () => {
     wrapper.setProps({isAuthenticated: true});
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
-});
 
+  it('should render an exact logout button', () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+  });
+});
